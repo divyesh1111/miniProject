201b84366d27301d1bbcd45bfa5570861df66818
divyesh1111 <divyeshhirpara000@gmail.com>
2023-09-29 20:24:46 +0530

    k

--- frontend/pages/category/[slug].js
+++ frontend/pages/category/[slug].js
@@ -38,6 +38,7 @@ const maxResult = 9;
                 <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
                 {data?.data?.map((product) => (
 
+                    
 <ProductCard key={product?.id} data={product} />
 ))};
 
